92dfba0794d424d71fd5e9d6f0334e61108a7d18
codesmoo <codesmoo@gmail.com>
2025-04-03 19:27:44 +0900

    Add factorial function

--- calculator.py
+++ calculator.py
@@ -17,6 +17,12 @@ def get_Abs(num):
   else:
     return -num
 def get_Percent(a, b):
-    return (a/b) * 100
+  return (a/b) * 100
 def get_Sum_ver1(n):
   return n(n-1)/2
+def factorial(n):
+  num = 1
+  while n >= 1:
+    num = num * n
+    n = n - 1
+  return num
